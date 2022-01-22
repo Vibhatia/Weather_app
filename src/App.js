b70d3b2042adcb20de7ac15190cf298ab2bc1bce
Vikas Bhatia <bhatiavikas2001@gmail.com>
2022-01-22 22:21:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+ import React,{useState,useEffect} from 'react';
+ import Info from './Info';
 
-function App() {
+ import './index.css';
+
+ import Mist from "./images/mist.jpg";
+import Sunny from "./images/sunny.jpg";
+const App = () => {
+  const [searchCity,setSearchCity] =useState('delhi');
+  const [cityInfo,setCityInfo]=useState({});
+  //  const [value,setValue]=useState({});
+
+  // const Fetch = () =>  {
+  //    fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=c44850806df28add00cfc1c7148bf695`
+  //   )
+  //     .then((response) => response.json())
+  //     .then(data => console.log(data))
+  //     .then(data => 
+  //       setCityInfo(data)
+        
+  //       );
+  //   };
+
+
+  const Fetch = async () =>{
+    let response=await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=c44850806df28add00cfc1c7148bf695`)
+    let data = await response.json();
+    console.log(data);
+    setCityInfo(data);
+
+    setSearchCity("");
+  }
+
+  useEffect(() => {
+    Fetch();
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  
+   {/* <div className={(typeof cityInfo != "undefined") ? ((cityInfo.main.temp > 0) ? 'warm' : 'app') : 'app'}>
+</div>  */}
+      {/* <div className="app" style={ 
+         cityInfo.weather[0].main==="Mist"? {backgroundImage: `url(${Mist})` } : { backgroundImage: `url(${Sunny})` }
+      }> */}
+
+     
+    <div className="search">
+      <input type = "search"
+       placeholder="Search your city...."
+       autoFocus
+       value={searchCity}
+       onChange={(e) => setSearchCity(e.target.value)}
+      />
+       <button className="searchButton"onClick={Fetch}>Search</button>
     </div>
-  );
+   
+   
+    { <Info cityInfo = {cityInfo}/> }
+    {/* </div> */}
+    </>
+    
+  )
 }
 
-export default App;
+export default App
